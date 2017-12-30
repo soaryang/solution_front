f82c9be80d9eb692fb6623a397f1a82e74fa7833
soaryang <759620299@qq.com>
2017-12-30 17:10:30 +0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -80,9 +80,9 @@ router.get("/login/githubLogin", function (req, res, next) {
             console.log('body======' + body);
             var urlStr = githubConfig.user_info_url + body;
             var options2 = {
-                'url': urlStr
-
-            }
+                'url': urlStr,
+                'User-Agent': "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2;.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0;InfoPath.3; KB974488)"
+        }
             console.log('url:'+urlStr);
             request(options2, function (error, response, resbody) {
                 if (!error) {
